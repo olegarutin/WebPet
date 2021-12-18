6c6d1ecf1594885dfaa9d2c52688655d90e61ab2
oleg <o1eg.34ac@gmail.com>
2021-12-18 22:44:32 +0200

    Add server response

--- app/tamagotchi.rb
+++ app/tamagotchi.rb
@@ -17,14 +17,31 @@ class Pet
   end
 
   def response
-    #Class: Rack::Response
+    case @request.path
+    when '/'
+      Rack::Response.new(render("form.html.erb"))
+    when '/initialize'
+      Rack::Response.new do |response|
+        response.set.cookies('food', @food)
+        response.set.cookies('water', @water)
+        response.set.cookies('happy', @happy)
+        response.set.cookies('energy', @energy)
+        response.set.cookies('skills', @skills)
+        response.set.cookies('sleep', @sleep)
+        response.redirect('/index')
+    when '/index'
+      if [@food, @water, @energy, @happy, @sleep].one?(&:negative?)
+        Rack::Response.new('Game Over', 404)
+        Rack::Response.new(render("game_over.html.erb"))
+      elsif @skills >= 50
+        Rack::Response.new('End Game', 404)
+        Rack::Response.new(render("end_game.html.erb"))
+      else
+        Rack::Response.new(render("index.html.erb"))
+      end
   end
   
   def render(layout)
-    # File.expand_path("../../lib/mygem.rb", __FILE__)
-    # ERB.new(File.read(filename)
-    # template = ERB.new
-    # puts template.result(binding)
     template = File.expand_path("..views/#{layout}", __FILE__)
     ERB.new(File.read(template)).result(binding)
   end
