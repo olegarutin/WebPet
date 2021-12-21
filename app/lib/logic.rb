d2824d52cb1dbf0cf7b82b270ba404adefc12d42
oleg <o1eg.34ac@gmail.com>
2021-12-21 12:24:49 +0200

    Fix mistake

--- app/lib/logic.rb
+++ app/lib/logic.rb
@@ -3,7 +3,11 @@ module Logic
     Rack::Response.new do |response|
       needs.each { |param| response.set_cookie(param, request.cookies[param.to_s].to_i - rand(2)) }
 
-      request.cookies[need.to_s].to_i < 16 ? response.set_cookie(need, request.cookies[need.to_s].to_i + 4) : response.set_cookie(need, 20)
+      if request.cookies[need.to_s].to_i < 16
+        response.set_cookie(need, request.cookies[need.to_s].to_i + 4)
+      else
+        response.set_cookie(need, 20)
+      end
 
       response.redirect('/index')
     end
