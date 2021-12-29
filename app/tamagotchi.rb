f26606a3aaec558d0216f067499565298975f495
oleg <o1eg.34ac@gmail.com>
2021-12-29 19:16:54 +0200

    Improve readability

--- app/tamagotchi.rb
+++ app/tamagotchi.rb
@@ -21,7 +21,7 @@ class Pet
   def response
     case @request.path
     when '/'
-      Rack::Response.new(render('form.html.erb'))
+      render_page(render('form.html.erb'))
 
     when '/initialize'
       Rack::Response.new do |response|
@@ -36,32 +36,32 @@ class Pet
 
     when '/index'
       if [get('food'), get('water'), get('happy'), get('energy')].one?(&:negative?)
-        Rack::Response.new('Game Over', 404)
-        Rack::Response.new(render('end.html.erb'))
+        status_code('Game Over')
+        render_page(render('end.html.erb'))
       elsif get('power').to_i >= 20
-        Rack::Response.new('You win', 404)
-        Rack::Response.new(render('win.html.erb'))
+        status_code('You win')
+        render_page(render('win.html.erb'))
       elsif get('power').to_i <= 0
-        Rack::Response.new('You lose', 404)
-        Rack::Response.new(render('low_power.html.erb'))
+        status_code('You lose')
+        render_page(render('low_power.html.erb'))
       else
-        Rack::Response.new(render('index.html.erb'))
+        render_page(render('index.html.erb'))
       end
 
     when '/change'
-      return Logic.change_params(@request, 'food', @params) if @request.params['food']
-      return Logic.change_params(@request, 'water', @params) if @request.params['water']
-      return Logic.change_params(@request, 'happy', @params) if @request.params['happy']
+      return Logic.change_params(@request, 'food', @params)   if @request.params['food']
+      return Logic.change_params(@request, 'water', @params)  if @request.params['water']
+      return Logic.change_params(@request, 'happy', @params)  if @request.params['happy']
       return Logic.change_params(@request, 'energy', @params) if @request.params['energy']
-      return Logic.change_params(@request, 'power', @params) if @request.params['power']
+      return Logic.change_params(@request, 'power', @params)  if @request.params['power']
 
     when '/exit'
-      Rack::Response.new('Game Over', 404)
-      Rack::Response.new(render('end.html.erb'))
+      status_code('Game Over')
+      render_page(render('end.html.erb'))
 
     when '/simple'
-      Rack::Response.new('You win?', 404)
-      Rack::Response.new(render('simple_finish.html.erb'))
+      status_code('You win?')
+      render_page(render('simple_finish.html.erb'))
     end
   end
 
@@ -72,5 +72,13 @@ class Pet
 
   def get(param)
     param == 'name' ? @request.cookies['name'] : @request.cookies[param.to_s].to_i
+  end
+
+  def render_page(layout)
+    Rack::Response.new(layout)
+  end
+
+  def status_code(message, status: 404)
+    Rack::Response.new(message)
   end
 end
